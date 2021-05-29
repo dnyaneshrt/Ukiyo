8536f9c5c5a0173ba7e5d4c6ac2a844aaf0f284a
Dnyaneshwar <dnyaneshrt>
2021-05-29 10:51:42 +0530

    Whatsapp button issue resolved

--- app/src/main/java/com/tech/ukiyo/MainActivity.kt
+++ app/src/main/java/com/tech/ukiyo/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.tech.ukiyo
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
@@ -23,30 +26,78 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (isOnline()) {
 
-        webview.settings.javaScriptEnabled = true
-        webview.settings.builtInZoomControls = true
-        webview.settings.allowContentAccess = true
-        webview.settings.allowContentAccess = true
-        webview.settings.allowFileAccess = true
-        webview.settings.userAgentString =
-            "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3"
+            webview.settings.javaScriptEnabled = true
+            webview.settings.builtInZoomControls = true
+            webview.settings.allowContentAccess = true
+            webview.settings.allowContentAccess = true
+            webview.settings.allowFileAccess = true
+            webview.settings.userAgentString =
+                "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3"
 
-        webview.webViewClient = object : WebViewClient() {
-            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-                super.onPageStarted(view, url, favicon)
-                progressBar.visibility = View.VISIBLE
-                Toast.makeText(this@MainActivity,url,Toast.LENGTH_LONG).show()
+            webview.webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    progressBar.visibility = View.VISIBLE
+
+                }
+
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+
+                    if(url!!.startsWith("tel:") || url.contains("phone")) {
+                        var intent =Intent(Intent.ACTION_VIEW);
+                        intent.data= Uri.parse(url)
+                        startActivity(intent)
+                        return true
+                    }
+                    return super.shouldOverrideUrlLoading(view, url)
+                }
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    progressBar.visibility = View.GONE
+                }
+
+
+            }
+            webview.loadUrl(url)
+        } else {
+            try {
+                var alertDialog = AlertDialog.Builder(this).create()
+                alertDialog.setTitle("Info");
+                alertDialog.setMessage("Internet not available, Cross check your internet connectivity and try again")
+                alertDialog.setIcon(android.R.drawable.ic_dialog_alert);
+                var listener = DialogInterface.OnClickListener { dialog, which ->
+                    if (which == DialogInterface.BUTTON_POSITIVE) {
+                        dialog.dismiss()
+                        finish()
+                    }
+                }
+
+                alertDialog.setButton("ok", listener)
+                alertDialog.show();
+            } catch (e: Exception) {
+                Toast.makeText(this, e.message, Toast.LENGTH_LONG).show();
+
             }
 
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                progressBar.visibility = View.GONE
-            }
+        }
 
 
-  }
-        webview.loadUrl(url)
+
+    }
+
+    fun isOnline(): Boolean {
+        var conMgr = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var netInfo = conMgr.activeNetworkInfo
+
+        if (netInfo == null || !netInfo.isConnected() || !netInfo.isAvailable()) {
+            Toast.makeText(this, "No Internet connection!", Toast.LENGTH_LONG).show();
+            return false;
+        }else
+
+            return true
+
     }
     override fun onBackPressed() {
 
